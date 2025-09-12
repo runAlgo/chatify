14e6cc77fbd3d30b91ae44c48ca5b44c867af896
runAlgo <150834861+runAlgo@users.noreply.github.com>
2025-09-12 15:48:23 +0545

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,28 @@
-import express from "express"
-import dotenv from "dotenv"
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.route.js"
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import path from "path";
 
-dotenv.config()
+dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT || 3000
+const __dirname = path.resolve();
 
-app.use("/api/auth", authRoutes)
-app.use("/api/messages", messageRoutes)
+const PORT = process.env.PORT || 3000;
 
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+// make ready for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running at port:${PORT}`)
-})
+  console.log(`Server is running at port:${PORT}`);
+});
