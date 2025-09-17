b535763694c1e6be8476274ca0e9f7211676c28d
runAlgo <150834861+runAlgo@users.noreply.github.com>
2025-09-17 17:20:59 +0545

    Update profile picture endpoint to exclude password from response

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -133,7 +133,7 @@ export const updateProfile = async (req, res) => {
     const userId = req.user._id;
 
     const uploadResponse = await cloudinary.uploader.upload(profilePic)
-    const updatedUser = await User.findByIdAndUpdate(userId, {profilePic: uploadResponse.secure_url}, {new: true})
+    const updatedUser = await User.findByIdAndUpdate(userId, {profilePic: uploadResponse.secure_url}, {new: true}).select("-password")
     if(!updatedUser) return res.status(404).json({message: "User not found"})
       res.status(200).json(updatedUser)
   } catch (error) {
